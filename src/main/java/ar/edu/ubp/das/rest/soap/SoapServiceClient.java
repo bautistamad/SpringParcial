2e601d79095651107f8510e30ce30cf2a8a2cade
Bautista Madruga <bmadruga@wilab.io>
2023-11-15 01:36:21 -0300

    rdy

--- src/main/java/ar/edu/ubp/das/rest/soap/SoapServiceClient.java
+++ src/main/java/ar/edu/ubp/das/rest/soap/SoapServiceClient.java
@@ -10,12 +10,11 @@ import ar.edu.ubp.das.rest.beans.Tipos;
 @Component
 public class SoapServiceClient implements SoapServiceClientImpl{
 
-	private static final String SERVICE_URL = "http://localhost:8085/ParcialWS/services/ParcialWSPort?wsdl";
+	private static final String SERVICE_URL = "http://localhost:8080/ServicioParcialWS/services/TiposServiciosWSPort?wsdl";
 	
 	private final SoapServiceClientImpl soapServiceClientImpl;
 	
 	public SoapServiceClient() {
-        // Configurar el proxy aquí, evitando crear una nueva instancia de SoapServiceClient
         JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
         factory.setServiceClass(SoapServiceClientImpl.class);
         factory.setAddress(SERVICE_URL);
@@ -26,8 +25,6 @@ public class SoapServiceClient implements SoapServiceClientImpl{
 	@Override
 	public List<Tipos> getTiposServicios() {
 		
-
-        // Llama al método en el servicio SOAP
         return soapServiceClientImpl.getTiposServicios();
 	}
 
